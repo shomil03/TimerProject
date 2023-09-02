a79126c7ec18a33b74a8f1998a3a109b88992334
shomil03 <shomil02020@gmail.com>
2023-09-03 00:22:15 +0530

    Initial Commit

---
+++ TimerProject/TimerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerProjectApp.swift
+//  TimerProject
+//
+//  Created by Shomil Singh on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
